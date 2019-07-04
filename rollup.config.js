1ff71e5095f0423e0749a1d870ecd3765b358acf
jw397 <jw397@126.com>
2019-07-04 15:45:52 +0800

    feat: ts模板

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import {uglify} from 'rollup-plugin-uglify'; //压缩bundle文件
 const isProduction = process.env.NODE_ENV === 'production';
 const isCommon = process.env.IS_COMMON === 'true';//是否只用公用模块，非浏览器
 
-const input = isCommon ? 'src/common/index.ts' : 'src/browser/index.ts';
+const input = isCommon ? 'src/XX/index.ts' : 'src/index.ts';
 
 export default {
   input: input,
